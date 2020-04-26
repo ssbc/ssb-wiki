a28b813ca34c6e1adcf07b155cd96270754a98e8
Jacob Karlsson <jacob.karlsson95@gmail.com>
2020-04-26 23:50:26 +0200

    Add functions for making edits and merges

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 const pull = require('pull-stream')
+const _ = {
+  get: require('lodash.get')
+}
 const { getHeads } = require('ssb-tangle/lib/misc-junk')
 
 module.exports = server => ({
@@ -8,13 +11,49 @@ module.exports = server => ({
     }
   },
 
-  getHeads: async root => {
-    return getTangleMembers(server, root)
+  newEdit: (rootKey, previousKey, content) => {
+    // todo: use one of those fancy functions that checks that it looks like an
+    // actual ssb link instead
+    if (typeof previousKey !== 'string') throw 'previousKey has to be a reference to the message being edited: ' + previousKey
+
+    return newEditOrMerge(rootKey, [previousKey], content)
+  },
+
+  newMerge: (rootKey, previousKeys, content) => {
+    if (previousKeys.length < 2) throw 'A merge needs 2 messages or more: ' + previousKeys
+
+    return newEditOrMerge(rootKey, previousKeys, content)
+  },
+
+  getHeads: async rootKey => {
+    return getTangleMembers(server, rootKey)
     // todo filter members for heads only
   }
 })
 
-async function getTangleMembers (server, root) {
+function newEditOrMerge (rootKey, previousKeys, content) {
+  // all messages in `previousKeys` (an array of keys) should have the same root
+  // the only difference between a normal edit and a merge is:
+  // in a normal edit, previousKeys.length === 1
+  // in a merge, previousKeys.length > 1
+
+  return {
+    type: 'wikiwiki',
+    // how do mention indexing work? will they be detected if `content` is
+    // content: {
+    //   text: 'example',
+    //   mentions: [...]
+    // }
+    // or does the mentions array need to be top level in the message?
+    content: content,
+    tangle: {
+      root: rootKey,
+      previous: previousKeys
+    }
+  }
+}
+
+async function getTangleMembers (server, rootKey) {
   return new Promise((resolve, reject) => {
     pull(
       // first find everything that isn't the root
@@ -24,7 +63,7 @@ async function getTangleMembers (server, root) {
             content: {
               type: 'wikiwiki',
               tangle: {
-                root: root
+                root: rootKey
               }
             }
           }
@@ -34,7 +73,7 @@ async function getTangleMembers (server, root) {
         if (err) return reject(err)
 
         // then get the root
-        server.get(root, (err, rootMsg) => {
+        server.get({ id: rootKey, meta: true }, (err, rootMsg) => {
           if (err) return reject(err)
 
           replies.push(rootMsg)
--- tests/index.test.js
+++ tests/index.test.js
@@ -21,11 +21,17 @@ test('wiki', async t => {
   console.log(rootMsg)
   const rootKey = rootMsg.key
 
-  console.log(await publish(bobby, {type: 'test', content: 'a normal message'}))
+  console.log(await publish(bobby, { type: 'test', content: 'a normal message just because' }))
 
-  const heads = await ssbWiki.getHeads(rootKey)
+  let heads = await ssbWiki.getHeads(rootKey)
+  fancylog(await publish(alicia, ssbWiki.newEdit(rootKey, heads[0].key, {
+    text: 'How 2 cook potatoes',
+    mentions: [{ link: '&KFoVS0P4Ps7BQzfBn3IOqV/Ujc9j8XAip5t01tRj74Y=.sha256'}]
+  })))
 
-  console.log('heads (not heads yet)', JSON.stringify(heads, null, 2))
+  heads = await ssbWiki.getHeads(rootKey)
+  console.log('heads2')
+  fancylog(heads)
 
   myTempSbot.close()
 
@@ -40,4 +46,8 @@ async function publish (user, message) {
       return resolve(data)
     })
   })
+}
+
+function fancylog (obj) {
+  console.log(JSON.stringify(obj, null, 2))
 }
